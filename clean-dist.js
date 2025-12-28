8ed0b41e83a968173de42a1ee8104c2de9c77388
Daniel Kimmich <json-derulo@users.noreply.github.com>
2025-12-29 00:58:14 +0100

    chore(deps): update dependencies

--- clean-dist.js
+++ clean-dist.js
@@ -1,11 +1,11 @@
-import { rm } from 'node:fs/promises';
-import { resolve } from 'node:path';
+import { rm } from "node:fs/promises";
+import { resolve } from "node:path";
 
-const distPath = resolve('dist');
+const distPath = resolve("dist");
 
 try {
-  await rm(distPath, { recursive: true, force: true });
-  console.log('dist folder removed');
+	await rm(distPath, { recursive: true, force: true });
+	console.log("dist folder removed");
 } catch (err) {
-  if (err.code !== 'ENOENT') throw err; // Ignore if folder doesn't exist
+	if (err.code !== "ENOENT") throw err; // Ignore if folder doesn't exist
 }
